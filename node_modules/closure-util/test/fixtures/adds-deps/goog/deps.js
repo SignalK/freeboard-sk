5594cd07b1f32376bf7b10ed711c0adb7354f718
RBerliner <RBerliner@InstrumentationAssociates.com>
2017-03-15 14:03:16 -0400

    gitignore update

--- node_modules/closure-util/test/fixtures/adds-deps/goog/deps.js
+++
@@ -1,2 +0,0 @@
-goog.addDependency('foo/bar.js', ['goog.foo.bar'], []);
-goog.addDependency('foo/baz.js', ['goog.foo.baz'], []);
--- node_modules/knob/example/entry.js
+++
@@ -1,7 +0,0 @@
-window.Knob = require('../')
-
-window.append = function(node){ // allow script tags to append elements before themselves
-  var scripts = document.getElementsByTagName('script')
-  var script = scripts[scripts.length-1]
-  script.parentNode.insertBefore(node, script)
-}
