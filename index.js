1f43f41b86cbe529cbac40b53d80410dd3781764
Scott Bender <scott@scottbender.net>
2017-07-03 01:55:51 -0400

    Change order when looking for uuid, mmsi and add url

--- index.js
+++ index.js
@@ -168,12 +168,15 @@ $.ajax({
 			dataType: "json",
 			success: function (data) {
 				//var jsonData = JSON.parse(data);
-				//console.log(JSON.stringify(data));
-                          	if(data.mmsi){
-                        		window.ownVessel = "urn:mrn:imo:mmsi:" + data.mmsi;
-                                } else if (data.uuid) {
+			  	//console.log(JSON.stringify(data));
+                          	if (data.uuid) {
 					window.ownVessel = data.uuid;
-				}
+				} else if(data.mmsi){
+                        		window.ownVessel = "urn:mrn:imo:mmsi:" + data.mmsi;
+                                } else if(data.url)
+                          	{
+                                  	window.ownVessel = data.url;
+                          	}
 				if (typeof (Storage) !== "undefined") {
 					if (data.environment) {
 						localStorage.setItem("sparklinePoints", data.environment.depth.meta.sparkline.points.value);
