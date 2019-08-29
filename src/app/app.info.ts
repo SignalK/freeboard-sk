cda0a5b8bdb5cb773c1eec55b816196642b11116
Adrian Panazzolo <38519157+panaaj@users.noreply.github.com>
2019-08-29 12:16:04 +0930

    Bump version to 1.6.0

--- src/app/app.info.ts
+++ src/app/app.info.ts
@@ -53,7 +53,7 @@ export class AppInfo extends Info {
         this.name= "Freeboard";
         this.shortName= "freeboard";
         this.description= `Signal K Chart Plotter.`;
-        this.version= '1.5.2';
+        this.version= '1.6.0';
         this.url= 'https://github.com/signalk/freeboard-sk';
         this.logo= "./assets/img/app_logo.png";       
         
