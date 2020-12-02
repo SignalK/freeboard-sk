3c7d4fb0f77c3c74ef3acb07e57e16c11df3e127
Adrian <38519157+panaaj@users.noreply.github.com>
2020-12-02 14:38:45 +1030

    bump version 1.3.1

--- src/app/app.info.ts
+++ src/app/app.info.ts
@@ -127,7 +127,7 @@ export class AppInfo extends Info {
         this.name= "Freeboard";
         this.shortName= "freeboard";
         this.description= `Signal K Chart Plotter.`;
-        this.version= '1.13.0';
+        this.version= '1.13.1';
         this.url= 'https://github.com/signalk/freeboard-sk';
         this.logo= "./assets/img/app_logo.png";   
         
